b9ee392ab9738727fbc2fc77d52a9fab7c28e9eb
Hosung Kim <hermian@gmail.com>
2021-10-17 22:38:47 +0900

    add dollar

--- strategy.py
+++ strategy.py
@@ -17,12 +17,12 @@ class StatIDAverageMomentumScore(bt.Algo):
         selected = target.temp['selected'].copy()
         if self.cash in selected:
             selected.remove(self.cash) # ID Mementum을 구할때 제외하고 구해야 한다.방어코드
-        
+
         t0 = target.now - self.lag
         # prc = target.universe.loc[(t0 - self.lookback):t0,selected] # FIXME 12개월치의 데이터를 뽑는다.
         prc = target.universe.loc[t0-self.lookback:t0,selected]
         # print(t0, prc.iloc[-365:-30])
-        #6, 9, 12개월 평균모멘텀스코어
+        #6, 9, 12개월 수익률 평균 계산
         # print(prc.iloc[-183:-30])
 
         #2002.2.1 -> 2002.1.2 -> 2001.8.3 -> 2001.5.4 -> 2001.2.1
@@ -33,7 +33,7 @@ class StatIDAverageMomentumScore(bt.Algo):
         average_returns = (m6_returns+m9_returns+m12_returns)/3
         # print(f"{t0}\n===average_returns : \n{average_returns}\n")
 
-        # ID 계산 최근 30일 제외
+        # ID 계산
         # dropna에 주의 해야 한다. 조선이 0이 있어 문제가 되므로 모든 column이 nan일 때만 drop한다.
         # print(prc[-(365+30):])
         # print(f"{t0} len {len(prc[-(366+30):].pct_change(30).dropna(how='all'))}")
@@ -44,10 +44,10 @@ class StatIDAverageMomentumScore(bt.Algo):
 
         target.temp['stat'] = average_returns * ID * -1
         return True
-    
+
 class WeighEquallyWithoutCash(bt.Algo):
     ''' cash에 대한 비중을 별도로 받아서, cash를 제외한 selected는 cash 비중 빼고 동일 비중으로 할당한다.
-    
+
     :param cash_weights: 현금 비중 데이터 프레임 (다른 비중이 같이 있어도 된다. cash 변수 값을 열이름으로 사용)
     :param cash: cash 자원의 이름 (ex, '현금', 'AGG', 'SHY'등)
     :param lag: cash_weights 데이터프레임에서 비중을 추출할때 얼마만큼 lag를 할 것인지를 결정
@@ -63,17 +63,17 @@ class WeighEquallyWithoutCash(bt.Algo):
         if self.cash in selected:
             raise ValueError(f"selected 자산에 {self.cash}는 없어야 합니다.")
             # ID Mementum을 구할때 제외하고 구해야 한다. (방어코드)
-        
-        t0 = target.now - self.lag       
+
+        t0 = target.now - self.lag
         cash_weight = self.cash_weights.loc[t0, self.cash]
         others_weights= 1.0 - cash_weight # selected의 비중
 
         w = others_weights/len(selected) # 동일비중
         weights = {x: w for x in selected}
-        weights[self.cash] = cash_weight # 현금지중추가
-        
+        weights[self.cash] = cash_weight # 현금비중추가
+
         target.temp['weights'] = weights
         target.temp['selected'] = selected + [self.cash] #selected에 현금 추가
 
         return True
-    
+
